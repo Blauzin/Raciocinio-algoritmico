50c73d6087331abbce6da852b0c3802f99f3f9c6
Blauzin <matheusmgirardi@gmail.com>
2023-03-11 20:22:59 -0300

    Changed the code to use the currrent date and not an arbirtrary fixed date

--- exercício 1/age.py
+++ exercício 1/age.py
@@ -1,16 +1,17 @@
 import sys
+from datetime import date
 
 
 day = int(input("type the day you were born: "))
 month = int(input("type the month you were born: "))
 year = int(input("type the year you were born: "))
 
-age = int(2023 - year)
+age = int(date.today().year - year)
 
-if month == 3 and day == 7:
+if month == date.today().month and day == date.today().day:
     print(f"Happy birthday! You are {age} years old")
     sys.exit()
-if month >=3 and day >7:
+if month >=date.today().month and day >date.today().day:
     age = age - 1
     
     
