34c1eaf521ab6ac967e40c9be1718fe47e2e45aa
Blauzin <matheusmgirardi@gmail.com>
2023-03-22 10:00:26 -0300

    adicionado alguns cometários

--- TDE 3/6-tabela_pole_cm.py
+++ TDE 3/6-tabela_pole_cm.py
@@ -1,6 +1,6 @@
 print("Tabela de Conversão de Polegadas para Centímetros")
 print("Polegadas \t Centímetros") #\t é basicamente um tab para separar os valores no print.
-
+#for cria a variável polegada para a range 1 a 21
 for polegada in range(1, 21):
     cm = polegada * 2.54
     print("{:.1f} \t\t {:.2f}".format(polegada, cm))
