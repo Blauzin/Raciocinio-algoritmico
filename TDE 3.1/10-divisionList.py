634a171ae6b8795d23a742bdb3e684b978bda11b
Blauzin <62842925+Blauzin@users.noreply.github.com>
2023-04-18 08:53:49 -0300

    dont remember

---
+++ TDE 3.1/10-divisionList.py
@@ -0,0 +1,11 @@
+n = 1
+m = 1
+termos = int(input("Digite o número de termos "))
+listaDiv = []
+while termos > 0: 
+    div = m "/" n
+    listaDiv.append(f{n}"/"{m})
+    n += 1
+    m += 2
+    termos -= 1
+print(listaDiv)
---
+++ TDE 3.1/5-factorial.py
@@ -0,0 +1,11 @@
+numb = int(input("Digite um número para ser feito o fatorial: "))
+fatorial = 1
+for i in range(numb, 0, -1):
+    fatorial = fatorial * i
+print(fatorial)
+
+
+#while numb > 0:
+ #   fatorial = fatorial * numb
+  #  numb -= 1
+#print(fatorial)
